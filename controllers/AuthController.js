bd744ae754a18a7de79b7f6132c7aa4265b63bc0
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-11 16:55:33 -0400

    Vídeo Aula 5°

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,24 +1,75 @@
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcryptjs = require('bcryptjs');
 
 class AuthController {
 static async cadastro(req, res) {
-    const {nome, email, password, tipo} = req.body;
+    const {nome, email,  password} = req.body;
 
-    if(!nome || !email || !password || !tipo) {
+    if(!nome || !email || !password) {
         return res.status(400).json({
             error: true,
             mensagem: 'Preencha todos os campos'
         });
     }
+
+    if(!email || email.length < 5) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Email inválido'
+        });
+    }
+
+    if(!password || password.length < 8) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Senha inválido'
+        });
+    }
+
+    const existe = await prisma.usuario.count({
+        where: {
+            email: email
+        }
+    })
+
+    if(existe != 0) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Email já cadastrado'
+        });
+    }
+
+    const salt = bcryptjs.genSaltSync(10);
+    const hashPassword = bcryptjs.hashSync(password, salt);
     
+  
+
+    try{
+       const usuario = await prisma.usuario.create({
+            data: {
+                nome: nome,
+                email: email,
+                password: hashPassword,
+                tipo: "cliente"
+            }
+        })
+        console.log(usuario);
+        
         return res.json({
             error: false,
             mensagem: 'Usuario cadastrado',
-            token: "asdfas4545353"
         });
-    
-    
+
+        
+
+    }catch(error){
+        return res.json({
+            error: true,
+            mensagem: 'Erro ao cadastrar usuário' + error,
+        });
+    }
+       
 }
 
     static async login(req, res) {
