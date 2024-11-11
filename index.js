f8820ff4edd3e144f4808894b83b148c71a3913f
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-11 10:32:43 -0400

    começando
    começando outro projeto porque não tava entendendo nada então vou fazer do início

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const {PrismaClient} = require("@prisma/client");
-const prisma = new PrismaClient();
-
-const express = require("express");
-
-const app = express();
-app.use(express.json());
-
-const authRoutes = require("./routes/authRoutes");
-app.get("/auth", authRoutes);
-
-app.listen(8000);
+const {PrismaClient} = require('@prisma/client')
