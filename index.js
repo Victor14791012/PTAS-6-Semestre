e8678c7babb1179047047f07c36241bcfb31a8e1
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-11 10:44:05 -0400

    1° Vídeo Aula :-)

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const {PrismaClient} = require('@prisma/client')
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+    const usuario = await prisma.usuario.create({
+        data: {
+            nome: 'Victor',
+            email: 'victor@example.com',
+            password: '123456',
+            tipo: 'cliente'
+        },
+    });
+    console.log("Novo Usuário: "+ JSON.stringify(usuario));
+
+    const usuarios = await prisma.usuario.findMany();
+    console.log("Usuários: ");
+    console.log(usuarios);
+
+}
+
+main()
