28403134a038660572c55e7b8562ee6d2553f01c
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-12 23:56:07 -0400

    aula 6

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken")
 
 class AuthController {
 static async cadastro(req, res) {
@@ -55,10 +56,18 @@ static async cadastro(req, res) {
             }
         })
         console.log(usuario);
+
+        const token = jwt.sign({
+            id: usuario.id,
+        }, process.env.SECRET_KEY, {
+            expiresIn: '1d'
+        });
+    
         
         return res.json({
             error: false,
             mensagem: 'Usuario cadastrado',
+            token: token
         });
 
         
@@ -72,9 +81,68 @@ static async cadastro(req, res) {
        
 }
 
-    static async login(req, res) {
-        res.send('Login');
+static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Preencha todos os campos'
+        });
     }
+
+    if (email.length < 5) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Email inválido'
+        });
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Senha curta'
+        });
+    }
+
+    const usuario = await prisma.usuario.findUnique({
+        where: {
+            email: email
+        }
+    });
+
+    if (!usuario) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Usuário não cadastrado'
+        });
+    }
+
+    const senhaCorreta =  bcryptjs.compareSync(password, usuario.password);
+
+    if (!senhaCorreta) {
+        return res.status(400).json({
+            error: true,
+            mensagem: 'Senha incorreta'
+        });
+    }
+
+// const senhaCorreta = await bcryptjs.compare(password, usuario.password);
+
+
+    const token = jwt.sign({
+        id: usuario.id,
+    }, process.env.SECRET_KEY, {
+        expiresIn: '1d'
+    });
+
+    return res.json({
+        error: false,
+        mensagem: 'Usuário logado',
+        token: token
+    });
+}
+
 }
 
 module.exports = AuthController
