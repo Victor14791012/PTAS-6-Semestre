6094b0565ede4d7a908609629f290ea85dded1bc
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-16 14:41:48 -0400

    cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const prisma = require('./prisma/prismaClient');
-
 const express = require('express');
-const app = express();
-app.use(express.json());
-
+const cors = require('cors'); 
+const prisma = require('./prisma/prismaClient');
 const authRoutes = require('./routes/authRoutes');
 
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true 
+}));
+
+app.use(express.json());
 app.use('/auth', authRoutes);
 
-app.listen(8000);
+app.listen(8000, () => {
+  console.log('Servidor rodando na porta 8000');
+});
